6e7c534d19a3a9661294fbc5442d13290fc8a0f7
Adriano Santos <sleipnir@bsd.com.br>
2022-01-05 18:01:19 -0300

    Adjust some options

--- src/main.rs
+++ src/main.rs
@@ -13,9 +13,8 @@ struct Cli {
 }
 
 #[derive(Subcommand)]
-enum Commands {
-    /// Create Eigr Function
-    Create {
+enum CreateCommands {
+    Function {
         /// name of function
         name: String,
 
@@ -31,9 +30,37 @@ enum Commands {
         /// set cpu limit of function
         cpu: Option<String>,
     },
+    StatefulStore {
+        /// name of stateful store
+        name: String,
+    },
+}
+
+#[derive(Subcommand)]
+enum DeleteCommands {
+    Function {
+        /// name of function
+        name: String,
+    },
+    StatefulStore {
+        /// name of stateful store
+        name: String,
+    },
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    /// Create Eigr Functions Resources
+    Create {
+        #[clap(subcommand)]
+        create: CreateCommands,
+    },
 
     /// Delete Eigr Function
-    Delete { name: String },
+    Delete {
+        #[clap(subcommand)]
+        delete: DeleteCommands,
+    },
 
     /// Expose function to outside the cluster
     Expose {
@@ -67,56 +94,85 @@ fn main() {
     let cli = Cli::parse();
 
     match cli.command {
-        Commands::Create {
-            name,
-            image,
-            replicas,
-            memory,
-            cpu,
-        } => {
-            let replicas = replicas.unwrap_or(1);
-            let memory = memory.unwrap_or("128Mi".to_string());
-            let cpu = cpu.unwrap_or("100m".to_string());
-            let namespace = cli.namespace;
-            let name = name;
-            let image = image;
-            let replicas = replicas;
-            let memory = memory;
-            let cpu = cpu;
-            /*create(
-                &namespace,
-                &name,
-                &image,
+        Commands::Create { create } => match create {
+            CreateCommands::Function {
+                name,
+                image,
                 replicas,
-                &memory,
-                &cpu,
-            );*/
-        }
-        Commands::Delete { name } => {
-            let namespace = cli.namespace;
-            let name = name;
-            //delete(&namespace, &name);
-        }
+                memory,
+                cpu,
+            } => {
+                let replicas = replicas.unwrap_or(1);
+                let memory = memory.unwrap_or("128Mi".to_string());
+                let cpu = cpu.unwrap_or("100m".to_string());
+                let _namespace = cli.namespace;
+                let _name = name;
+                let _image = image;
+                let _replicas = replicas;
+                let _memory = memory;
+                let _cpu = cpu;
+                /*create(
+                    &namespace,
+                    &name,
+                    &image,
+                    replicas,
+                    &memory,
+                    &cpu,
+                );*/
+            }
+            CreateCommands::StatefulStore { name } => {
+                let _namespace = cli.namespace;
+                let _name = name;
+                /*create(
+                    &namespace,
+                    &name,
+                    "eigr-stateful-store",
+                    1,
+                    "128Mi",
+                    "100m",
+                );*/
+            }
+        },
+        Commands::Delete { delete } => match delete {
+            DeleteCommands::Function { name } => {
+                let _namespace = cli.namespace;
+                let _name = name;
+                /*delete(
+                    &namespace,
+                    &name,
+                    "eigr-function",
+                );*/
+            }
+            DeleteCommands::StatefulStore { name } => {
+                let _namespace = cli.namespace;
+                let _name = name;
+                /*delete(
+                    &namespace,
+                    &name,
+                    "eigr-stateful-store",
+                );*/
+            }
+        },
         Commands::Expose {
             name,
             r#type,
             ingress_class_name,
         } => {
-            let namespace = cli.namespace;
-            let name = name;
-            let expose_type = r#type.unwrap_or("ingress".to_string());
-            let ingress_class_name = ingress_class_name.unwrap_or("nginx".to_string());
+            let _namespace = cli.namespace;
+            let _name = name;
+            let _expose_type = r#type.unwrap_or("ingress".to_string());
+            let _ingress_class_name = ingress_class_name.unwrap_or("nginx".to_string());
             //expose(&namespace, &name, &expose_type, &ingress_class_name);
         }
         Commands::Get { name, all, output } => {
-            let namespace = cli.namespace;
-            let name = name.unwrap_or("".to_string());
-            let all = all.unwrap_or(false);
-            let output = output.unwrap_or("".to_string());
+            let _namespace = cli.namespace;
+            let _name = name.unwrap_or("".to_string());
+            let _all = all.unwrap_or(false);
+            let _output = output.unwrap_or("".to_string());
             //get(&namespace, &name, all, &output);
         }
         Commands::Install { version } => {
-            let version = version.unwrap_or("".to_string());
+            let _version = version.unwrap_or("".to_string());
             //install(&version);
         }
     }
