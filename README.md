5488dd96266384044cc4680666178fbe114e4a40
Adriano Santos <sleipnir@bsd.com.br>
2022-01-05 16:01:48 -0300

    First options

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# Eigr Functions CLI
+# Eigr Functions CLI (or Ibex command line tool)
+
+## Explain why Ibex name
+
+The Alpine ibex is a mountain goat instantly recognizable by the male's large scimitar-shaped horns which are ridged in the front. Females grow a pair of smaller, thinner horns which develop considerably more slowly than those of the male. The ibex ranges in height from 27 to 43 inches at the shoulder and weighs between 200 to 270 pounds. Ibex are related to domestic goats and will mate with other goats if they are unable to find a mate in the wild. Ibex were hunted to extinction in the early 19th century. By 1856 only 60 Alpine ibex remained on a royal hunting reserve in the Grand Paradiso region of Italy. Some of these ibex were smuggled into Switzerland around 1900 in an effort to reinstate ibex in the Swiss Alps. Under protection, there are now over 40,000 ibex in the Swiss Alps. Ibex are frequently sighted in the most rocky and wild regions of the Swiss Alps along the Haute Route, especially on the Sentier de Chamois in the Upper Val de Bagnes Nature Reserve, where they were reintroduced in 1926, and around the Col de Louvie and the Col de Prafleuri.
+
+## Install
+TODO
+
+## Usage
+TODO
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,122 @@
+use clap::{AppSettings, Parser, Subcommand};
+
+#[derive(Parser)]
+#[clap(author, version, about)]
+#[clap(global_setting(AppSettings::PropagateVersion))]
+#[clap(global_setting(AppSettings::UseLongFormatForHelpSubcommand))]
+struct Cli {
+    /// Set the namespace for the command
+    #[clap(short, long, default_value = "eigr-functions")]
+    namespace: String,
+    #[clap(subcommand)]
+    command: Commands,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    /// Create Eigr Function
+    Create {
+        /// name of function
+        name: String,
+
+        /// container image of function
+        image: String,
+
+        /// set number of replicas of function
+        replicas: Option<u32>,
+
+        /// set memory limit of function
+        memory: Option<String>,
+
+        /// set cpu limit of function
+        cpu: Option<String>,
+    },
+
+    /// Delete Eigr Function
+    Delete { name: String },
+
+    Expose {
+        /// name of function
+        name: String,
+
+        /// set method to expose function [ingress, nodePort, loadBalancer]
+        r#type: Option<String>,
+
+        /// set ingressClassName if type is ingress [nginx]
+        ingress_class_name: Option<String>,
+    },
+
+    /// List Eigr Functions
+    Get {
+        /// name of function
+        name: Option<String>,
+
+        /// show all resources of function
+        all: Option<bool>,
+
+        /// choose the output format [json, yaml]
+        output: Option<String>,
+    },
+
+    /// Install Eigr k8s Operator
+    Install { version: Option<String> },
+}
+
 fn main() {
-    println!("Hello, world!");
+    let cli = Cli::parse();
+
+    match cli.command {
+        Commands::Create {
+            name,
+            image,
+            replicas,
+            memory,
+            cpu,
+        } => {
+            let replicas = replicas.unwrap_or(1);
+            let memory = memory.unwrap_or("128Mi".to_string());
+            let cpu = cpu.unwrap_or("100m".to_string());
+            let namespace = cli.namespace;
+            let name = name;
+            let image = image;
+            let replicas = replicas;
+            let memory = memory;
+            let cpu = cpu;
+            /*create(
+                &namespace,
+                &name,
+                &image,
+                replicas,
+                &memory,
+                &cpu,
+            );*/
+        }
+        Commands::Delete { name } => {
+            let namespace = cli.namespace;
+            let name = name;
+            //delete(&namespace, &name);
+        }
+        Commands::Expose {
+            name,
+            r#type,
+            ingress_class_name,
+        } => {
+            let namespace = cli.namespace;
+            let name = name;
+            let expose_type = r#type.unwrap_or("ingress".to_string());
+            let ingress_class_name = ingress_class_name.unwrap_or("nginx".to_string());
+            //expose(&namespace, &name, &expose_type, &ingress_class_name);
+        }
+        Commands::Get { name, all, output } => {
+            let namespace = cli.namespace;
+            let name = name.unwrap_or("".to_string());
+            let all = all.unwrap_or(false);
+            let output = output.unwrap_or("".to_string());
+            //get(&namespace, &name, all, &output);
+        }
+        Commands::Install { version } => {
+            let version = version.unwrap_or("".to_string());
+            //install(&version);
+        }
+    }
 }
